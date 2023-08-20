3645400cc344a351ae93ee00ad54478a9040a61d
osawa-koki <koki@osawa.dev>
2023-08-20 22:28:58 +0900

    一覧ページの所在を変えた。

--- client/pages/prefectures/index.tsx
+++ client/pages/prefectures/index.tsx
@@ -6,9 +6,9 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { gql } from '@apollo/react-hooks'
-import setting from '../setting'
-import PrefectureFilter from '../components/PrefectureFilter'
-import { type Prefecture } from '../src/gql/graphql'
+import setting from '../../setting'
+import PrefectureFilter from '../../components/PrefectureFilter'
+import { type Prefecture } from '../../src/gql/graphql'
 
 const client = new ApolloClient({
   link: new HttpLink({
