acb30ddc9706e8e74c93488159a4e734795b9f1a
osawa-koki <koki@osawa.dev>
2023-08-20 17:50:49 +0900

    不要なデータ型を削除。

--- server/src/@types/city.ts
+++
@@ -1,9 +0,0 @@
-interface City {
-  id: string
-  name: string
-  prefecture_id: number
-  population: number
-  area: number
-}
-
-export default City
--- server/src/@types/prefecture.ts
+++
@@ -1,9 +0,0 @@
-interface Prefecture {
-  id: number
-  name: string
-  capital: string
-  population: number
-  area: number
-}
-
-export default Prefecture
