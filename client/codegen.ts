94dc82458e9e7020506d1917cf2c7d828c0c9080
osawa-koki <koki@osawa.dev>
2023-08-20 20:27:30 +0900

    yarn graphql-codegen init

---
+++ client/codegen.ts
@@ -0,0 +1,16 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:8000",
+  documents: "**/*.tsx",
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      plugins: []
+    }
+  }
+};
+
+export default config;
